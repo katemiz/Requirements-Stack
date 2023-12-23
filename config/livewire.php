63635306c8548a0b3012817e48b84565e10a5bd2
Kılıç Ali Temiz <katemiz@gmail.com>
2023-12-23 18:23:35 +0300

    file upload max size increased; livewire

--- config/livewire.php
+++ config/livewire.php
@@ -103,7 +103,8 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
+        // 'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => 'max:102400', // (100MB max, and only accept PNGs, JPEGs, and PDFs)
         'directory' => null,   // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
